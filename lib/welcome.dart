88644cecfb8a8556788ad2e6e3103e7adcaa6e08
ayanameh1 <aya.nameh6@gmail.com>
2022-05-17 22:50:25 +0300

    566

--- lib/welcome.dart
+++ lib/welcome.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 
-
 class WelcomeScreen extends StatefulWidget {
   @override
   _WelcomeScreenState createState() => _WelcomeScreenState();
@@ -16,30 +15,36 @@ class _WelcomeScreenState extends State<WelcomeScreen> {
   Widget build(BuildContext context) {
     var sizeAware = MediaQuery.of(context).size;
     return Scaffold(
-        body: Column(
-          children: [
-            Container(
-              width: 500,
-              height: 500,
-              child: Center(
-                  child: Image(
-                    image: AssetImage('assets/images/Group 8.png'),
-                  ),
+      body: Column(
+        children: [
+          Container(
+            width: 500,
+            height: 500,
+            child: Center(
+              child: Image(
+                image: AssetImage('assets/images/Group 8.png'),
               ),
             ),
-            Container(
-              color: Colors.amber,
-              width: 100,
-              height: 50,
-              child: MaterialButton(
-                onPressed: (){},
-                  child: Text("Get Start",
-                  style: TextStyle(color: Colors.black,
-                  ),),
+          ),
+          Container(
+            width: 150,
+            height: 60,
+            child: MaterialButton(
+              onPressed: () {},
+              child: Text(
+                "Get Start",
+                style: TextStyle(
+                  color: Colors.black,
+                ),
               ),
-            )
-          ],
-        ),
+            ),
+            decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(20.0),
+                color: Colors.amber,
+                ),
+          ),
+        ],
+      ),
     );
   }
 }
