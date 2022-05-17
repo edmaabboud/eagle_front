59dc3af1014dd101ae08719558772c1f09f04112
Edma Abboud <abboudEdma@gmail.com>
2022-05-17 23:57:28 +0300

    .

--- lib/welcome.dart
+++ lib/welcome.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 
-
 class WelcomeScreen extends StatefulWidget {
   @override
   _WelcomeScreenState createState() => _WelcomeScreenState();
@@ -16,30 +15,59 @@ class _WelcomeScreenState extends State<WelcomeScreen> {
   Widget build(BuildContext context) {
     var sizeAware = MediaQuery.of(context).size;
     return Scaffold(
-        body: Column(
-          children: [
-            Container(
-              width: 500,
-              height: 500,
-              child: Center(
-                  child: Image(
-                    image: AssetImage('assets/images/Group 8.png'),
-                  ),
+      body: Column(
+        children: [
+          SizedBox(
+            height: MediaQuery.of(context).size.height * 663 / 1920,
+          ),
+          Center(
+            child: Image(
+              image: AssetImage('assets/images/Group 8.png'),
+              width: MediaQuery.of(context).size.width * 617 / 1080,
+              height: MediaQuery.of(context).size.height * 353 / 1920,
+            ),
+          ),
+          SizedBox(
+            height: MediaQuery.of(context).size.height * 134/ 1920,
+          ),
+          Center(
+            child: Text(
+              " Bring your exhibition event\n management to the next level\n with us",
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                color: Colors.black,
               ),
             ),
-            Container(
-              color: Colors.amber,
-              width: 100,
-              height: 50,
-              child: MaterialButton(
-                onPressed: (){},
-                  child: Text("Get Start",
-                  style: TextStyle(color: Colors.black,
-                  ),),
+          ),
+          SizedBox(
+            height: MediaQuery.of(context).size.height * 298/ 1920,
+          ),
+          Container(
+            width: MediaQuery.of(context).size.width * 359 / 1080,
+            height: MediaQuery.of(context).size.height * 82 / 1920,
+            child: MaterialButton(
+              onPressed: () {},
+              child: Text(
+                "Get Started",
+                style: TextStyle(
+                  color: Colors.black,
+                ),
               ),
-            )
-          ],
-        ),
+            ),
+            decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(54),
+                color: Color(0xffffee32),
+                boxShadow: [
+                  BoxShadow(
+                    color: Color(0xff565656),
+                    spreadRadius: 0,
+                    blurRadius: 0,
+                    offset: Offset(2,4),
+                  ),
+                ]),
+          )
+        ],
+      ),
     );
   }
 }
