b9ac2751f5ecfe2fc53062d856032d633b9e490c
Edma Abboud <abboudEdma@gmail.com>
2022-06-03 22:40:33 +0300

    swiper

--- lib/main.dart
+++ lib/main.dart
@@ -28,7 +28,7 @@ class MyApp extends StatelessWidget {
     ),
         ),
         ),
-      home: WelcomeScreen(),
+      home: loginscreen(),
     );
   }
 }
--- lib/ui/login.dart
+++ lib/ui/login.dart
@@ -14,10 +14,18 @@ class _loginscreenState extends State<loginscreen> {
   Widget build(BuildContext context) {
     var sizeAware = MediaQuery.of(context).size;
     bool _obscureText = true;
-     List <Bcard> cards = [
-      Bcard(photoname: 'assets/images/asset 1a.png', description: 'way to meet, communicate,\n share and compete '),
-      Bcard(photoname: 'assets/images/asset 4a.png', description: 'marketing for your work with\n the advantage of holding a\n conference to promote\n yourself .'),
-      Bcard(photoname:'assets/images/Asset 1.png', description: 'way to walk around the\n exhibition and keep up with the\n latest news and the biggest\n sales .')
+    List<Bcard> cards = [
+      Bcard(
+          photoname: 'assets/images/asset 1a.png',
+          description: 'way to meet, communicate,\n share and compete '),
+      Bcard(
+          photoname: 'assets/images/Asset 4a.png',
+          description:
+              'marketing for your work with\n the advantage of holding a\n conference to promote\n yourself .'),
+      Bcard(
+          photoname: 'assets/images/Asset 1.png',
+          description:
+              'way to walk around the\n exhibition and keep up with the\n latest news and the biggest\n sales .')
     ];
 
     return SafeArea(
@@ -30,130 +38,148 @@ class _loginscreenState extends State<loginscreen> {
             ),
             shadowColor: Colors.black.withOpacity(0),
           ),
-          body: Column(
-            children: [
-              // Swiper(
-              //   itemCount: cards.length,
-              //   itemBuilder: (context, index) {
-              //     return Bcard(photoname: cards[index].photoname, description: cards[index].description,);
-              //   },
-              //   control: SwiperControl(),
-              //   pagination: SwiperPagination(),
-              // ),
-              Container(
-                width: sizeAware.width * 764 / 1080,
-                height: sizeAware.height * 107 / 1920,
-                child: TextFormField(
-                  keyboardType: TextInputType.emailAddress,
-                  onFieldSubmitted: (String value) {
-                    print(value);
-                  },
-                  onChanged: (String value) {
-                    print(value);
-                  },
-                  validator: (String? value) {
-                    if (value == null || value.trim().length == 0) {
-                      return 'email must not be empty';
-                    }
-                    if (!RegExp("").hasMatch(value)) {
-                      return "Please Enter valid email ";
-                    }
-                    return null;
-                  },
-                  decoration: InputDecoration(
-                    hintText: 'email',
-                    hintStyle: TextStyle(
-                      fontFamily: 'Cremona',
-                    ),
-                    prefixIcon: Icon(
-                      Icons.email,
-                    ),
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.all(
-                        Radius.circular(54),
-                      ),
-                    ),
-                  ),
-                ),
-              ),
-              SizedBox(
-                height: sizeAware.height * 39 / 1920,
-              ),
-              Container(
-                width: sizeAware.width * 764 / 1080,
-                height: sizeAware.height * 107 / 1920,
-                child: TextFormField(
-                  keyboardType: TextInputType.visiblePassword,
-                  obscureText: _obscureText,
-                  onChanged: (String value) {
-                    print(value);
-                  },
-                  validator: (String? value) {
-                    if (value == null || value.trim().length == 0) {
-                      return " password must not be empty";
-                    }
-                    if (value.length < 8) {
-                      return " Password is too short";
-                    }
-                    return null;
-                  },
-                  decoration: InputDecoration(
-                    hintText: 'Password',
-                    hintStyle: TextStyle(
-                      fontFamily: 'Uniform',
-                    ),
-                    prefixIcon: const Icon(
-                      Icons.lock,
-                    ),
-                    suffixIcon: GestureDetector(
-                      onTap: () {
-                        setState(() {
-                          _obscureText = !_obscureText;
-                        });
+          body: Center(
+            child: SizedBox(
+              height: MediaQuery.of(context).size.height,
+              child: Column(
+                children: [
+                  Expanded(
+                    child: Swiper(
+                      containerHeight:
+                          MediaQuery.of(context).size.height * 898 / 1080,
+                      itemCount: cards.length,
+                      itemBuilder: (context, index) {
+                        return Bcard(
+                          photoname: cards[index].photoname,
+                          description: cards[index].description,
+                        );
                       },
-                      child: Icon(_obscureText
-                          ? Icons.visibility
-                          : Icons.visibility_off),
+                      //control: SwiperControl(),
+                      pagination: SwiperPagination(),
                     ),
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.all(
-                        Radius.circular(54),
+                  ),
+                  Container(
+                    width: sizeAware.width * 764 / 1080,
+                    height: sizeAware.height * 107 / 1920,
+                    child: TextFormField(
+                      keyboardType: TextInputType.emailAddress,
+                      onFieldSubmitted: (String value) {
+                        print(value);
+                      },
+                      onChanged: (String value) {
+                        print(value);
+                      },
+                      validator: (String? value) {
+                        if (value == null || value.trim().length == 0) {
+                          return 'email must not be empty';
+                        }
+                        if (!RegExp("").hasMatch(value)) {
+                          return "Please Enter valid email ";
+                        }
+                        return null;
+                      },
+                      decoration: InputDecoration(
+                        hintText: 'email',
+                        hintStyle: TextStyle(
+                          fontFamily: 'Uniform',
+                        ),
+                        prefixIcon: Icon(
+                          Icons.email,
+                        ),
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.all(
+                            Radius.circular(54),
+                          ),
+                        ),
                       ),
                     ),
                   ),
-                ),
+                  SizedBox(
+                    height: sizeAware.height * 39 / 1920,
+                  ),
+                  Container(
+                    width: sizeAware.width * 764 / 1080,
+                    height: sizeAware.height * 107 / 1920,
+                    child: TextFormField(
+                      keyboardType: TextInputType.visiblePassword,
+                      obscureText: _obscureText,
+                      onChanged: (String value) {
+                        print(value);
+                      },
+                      validator: (String? value) {
+                        if (value == null || value.trim().length == 0) {
+                          return " password must not be empty";
+                        }
+                        if (value.length < 8) {
+                          return " Password is too short";
+                        }
+                        return null;
+                      },
+                      decoration: InputDecoration(
+                        hintText: 'Password',
+                        hintStyle: TextStyle(
+                          fontFamily: 'Uniform',
+                        ),
+                        prefixIcon: const Icon(
+                          Icons.lock,
+                        ),
+                        suffixIcon: GestureDetector(
+                          onTap: () {
+                            setState(() {
+                              _obscureText = !_obscureText;
+                            });
+                          },
+                          child: Icon(_obscureText
+                              ? Icons.visibility
+                              : Icons.visibility_off),
+                        ),
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.all(
+                            Radius.circular(54),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    height: MediaQuery.of(context).size.height * 202 / 1920,
+                  ),
+                  Container(
+                    width: MediaQuery.of(context).size.width * 359 / 1080,
+                    height: MediaQuery.of(context).size.height * 82 / 1920,
+                    child: MaterialButton(
+                      onPressed: () {
+                        // Navigator.push(
+                        //   context,MaterialPageRoute(builder: (context) => SignUpScreen(),
+                        // ),
+                        // );
+                      },
+                      child: Text(
+                        "Login",
+                        style: TextStyle(
+                          color: black,
+                          fontFamily: 'Uniform',
+                        ),
+                      ),
+                    ),
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(54),
+                        color: yellow1,
+                        boxShadow: [
+                          BoxShadow(
+                            color: Color(0xff565656),
+                            spreadRadius: 0,
+                            blurRadius: 0,
+                            offset: Offset(2, 4),
+                          ),
+                        ]),
+                  ),
+                  SizedBox(
+                    height: MediaQuery.of(context).size.height * 200 / 1920,
+                  ),
+                ],
               ),
-              Container(
-                width: MediaQuery.of(context).size.width * 359 / 1080,
-                height: MediaQuery.of(context).size.height * 82 / 1920,
-                child: MaterialButton(
-                  onPressed: () {
-                    // Navigator.push(
-                    //   context,MaterialPageRoute(builder: (context) => SignUpScreen(),
-                    // ),
-                    // );
-                  },
-                  child: Text(
-                    "Login",
-                    style: TextStyle(
-                      color: black,
-                      fontFamily: 'Uniform',
-                    ),
-                  ),
-                ),
-                decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(54),
-                    color: yellow1,
-                    boxShadow: [
-                      BoxShadow(
-                        color: Color(0xff565656),
-                        spreadRadius: 0,
-                        blurRadius: 0,
-                        offset: Offset(2, 4),
-                      ),
-                    ]),
-              )
-            ],
+            ),
           )),
     );
   }
@@ -173,14 +199,37 @@ class _BcardState extends State<Bcard> {
   @override
   Widget build(BuildContext context) {
     return Container(
+      //color: yellow1,
+      // width: MediaQuery.of(context).size.width * 844 / 1080,
+      // height: MediaQuery.of(context).size.height * 700 / 1920,
       child: Column(
         children: [
-          Image(image: AssetImage(widget.photoname)),
-          Text(
-            'Quick & easy',textAlign: TextAlign.center,
-            style: TextStyle(fontFamily: 'Uniform Black',),
+          Image(
+            image: AssetImage(widget.photoname),
+            width: MediaQuery.of(context).size.width * 600 / 1080,
+            height: MediaQuery.of(context).size.height * 600 / 1920,
           ),
-          Text(widget.description),
+          // SizedBox(
+          //   height: MediaQuery.of(context).size.height * 2 / 1920,
+          // ),
+          Text(
+            'Quick & easy',
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              fontFamily: 'Uniform',
+              fontWeight: FontWeight.bold,
+              fontSize: 20,
+            ),
+          ),
+          SizedBox(
+            height: MediaQuery.of(context).size.height * 50 / 1920,
+          ),
+          Text(
+            widget.description,textAlign: TextAlign.center,
+            style: TextStyle(
+              fontFamily: 'Uniform',
+            ),
+          ),
         ],
       ),
     );
